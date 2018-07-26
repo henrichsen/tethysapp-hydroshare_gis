3428d31b461d9146a7903ae28ad39adcc9fa349c
kennethlippold <kjlippold@outlook.com>
2018-07-26 13:43:18 -0600

    Removed os dependency

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ app_class = 'hydroshare_gis.app:HydroshareGis'
 app_package_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tethysapp', app_package)
 
 # ### Python Dependencies ###
-dependencies = ['hs-restclient', 'rasterio', 'fiona', 'numpy', 'os']
+dependencies = ['hs-restclient', 'rasterio', 'fiona', 'numpy']
 
 setup(
     name='HydroShare GIS',
